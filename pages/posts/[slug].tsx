aefc008c8a1bba1de851b257487483d77c3bee7a
Kairi Yasumatsu <kairi.matibuse@gmail.com>
2022-09-06 09:54:18 +0900

    first commit

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -23,6 +23,7 @@ export default function Post({ post, morePosts, preview }: Props) {
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
   return (
     <Layout preview={preview}>
       <Container>
@@ -34,7 +35,7 @@ export default function Post({ post, morePosts, preview }: Props) {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {title}
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
